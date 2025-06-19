627297d5ccd2ae5ff8ffd7036667b4e5bb2df0ff
ucode <chantatha.p@gmail.com>
2025-06-19 10:22:10 +0700

    Add color theme

---
+++ client/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#89A8B2',
+    },
+    secondary: {
+      main: '#B3C8CF',
+    },
+    background: {
+      default: '#F1F0E8',
+      paper: '#E5E1DA',
+    },
+  },
+});
+
+export default theme;
