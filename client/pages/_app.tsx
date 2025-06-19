134f00ac3ac18fbe276c53c986448d5c7b55cccb
ucode <chantatha.p@gmail.com>
2025-06-19 15:11:15 +0700

    Wrap app with LocalizationProvider

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,17 +1,21 @@
 // @ts-nocheck
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import theme from '../theme';
 import { AuthProvider } from '../context/AuthContext';
 import '../styles/globals.scss';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </ThemeProvider>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </ThemeProvider>
+    </LocalizationProvider>
   );
 }
