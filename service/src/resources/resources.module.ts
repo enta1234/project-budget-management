29dc854600cd4c3fecfed25619a06afaf6b17bbc
ucode <chantatha.p@gmail.com>
2025-06-24 14:51:47 +0700

    Add full header export for resources

--- service/src/resources/resources.module.ts
+++ service/src/resources/resources.module.ts
@@ -4,9 +4,13 @@ import { ResourcesController } from './resources.controller';
 import { ResourcesService } from './resources.service';
 import { ResourcesRepository } from './data/resources.repository';
 import { Resource, ResourceSchema } from './data/resource.schema';
+import { PositionsModule } from '../positions/positions.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Resource.name, schema: ResourceSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Resource.name, schema: ResourceSchema }]),
+    PositionsModule,
+  ],
   controllers: [ResourcesController],
   providers: [ResourcesService, ResourcesRepository],
   exports: [ResourcesService],
