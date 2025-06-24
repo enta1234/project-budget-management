a27622ade5e19f8d12f6e0c0f8a55bebf15cf1e6
chantatha.p <chantatha.p@entronica.co.th>
2025-06-24 16:09:57 +0700

    fixed: form new project

--- client/components/ProjectForm.tsx
+++ client/components/ProjectForm.tsx
@@ -50,7 +50,7 @@ export default function ProjectForm({
 
   const handleSubmit = e => {
     e.preventDefault();
-    if (active < 2) return handleNext();
+    if (active < 3) return handleNext();
     if (onSubmit) {
       onSubmit({
         name,
@@ -279,7 +279,7 @@ export default function ProjectForm({
             Next
           </Button>
         ) : (
-          <Button type="submit" variant="contained">
+          <Button type="submit" variant="contained" onClick={handleSubmit}>
             {submitText}
           </Button>
         )}
