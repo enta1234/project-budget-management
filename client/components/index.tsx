375508e7d72d649cb0caaabe4b500a6104193cd3
chantatha.p <chantatha.p@entronica.co.th>
2025-06-27 17:02:16 +0700

    fixed: import

--- client/components/index.tsx
+++ client/components/index.tsx
@@ -22,7 +22,4 @@ export { default as ConfirmDialog } from './ConfirmDialog';
 export { default as PageBreadcrumbs } from './PageBreadcrumbs';
 export { default as PhaseForm } from './PhaseForm';
 export { default as TaskForm } from './TaskForm';
-
-export { default as RoleCard } from './RoleCard';
-
 export { default as RoleCard } from "./RoleCard";
