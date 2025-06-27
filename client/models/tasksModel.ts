2ce7a67f2582c100db8d2a1841a248245ec0a2e5
ucode <chantatha.p@gmail.com>
2025-06-27 12:36:55 +0700

    fix: sanitize planning data

--- client/models/tasksModel.ts
+++ client/models/tasksModel.ts
@@ -2,6 +2,12 @@ import api from '../api';
 
 export async function fetchTasks(projectId: string) {
   const { data } = await api.get('/api/v1/planning/tasks', { params: { project: projectId } });
-  return data;
+  return data.map((t: any) => {
+    if (typeof t.onClick !== 'undefined') {
+      const { onClick, ...rest } = t;
+      return rest;
+    }
+    return t;
+  });
 }
 
